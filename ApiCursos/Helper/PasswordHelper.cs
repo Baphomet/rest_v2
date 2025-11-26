19044a6c9eaf31eb2275eb4f9757bc401af7f4c9
Baphomet <fgrochovski@gmail.com>
2025-11-26 16:07:39 -0300

    feat: criaçao do PasswordHelper para criptografia de senhas com hash

---
+++ ApiCursos/Helper/PasswordHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ApiCursos.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static string Hash(string senha)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(senha));
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
+    }
+}
--- ApiCursos/obj/Debug/net8.0/ApiCursos.AssemblyInfo.cs
+++ ApiCursos/obj/Debug/net8.0/ApiCursos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCursos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a5e9ae10385a01fcc41de6617475950d5521b08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8bb863a6d8228d8b77144c123462c7fb1bfa39")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCursos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCursos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
